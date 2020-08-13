1abd4c1182d25388991f67b2c9e41fd6c5247509
raidipesh78 <raidipesh78@gmail.com>
2020-08-13 15:20:03 +0545

    Animated search init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AnimatedReaction from "./AnimatedReaction";
+
+ReactDOM.render(<AnimatedReaction />, document.querySelector("#root"));
